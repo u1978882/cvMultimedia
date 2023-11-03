2c8fa31b698b384104c4ab3ca46c746f299887f6
u1978882 <126769186+u1978882@users.noreply.github.com>
2023-11-03 17:26:44 +0100

    Skeleton done and progress bars

--- js/codi.js
+++ js/codi.js
@@ -1,11 +1,46 @@
 QuerrySelector.ready(function() {
     
+
+    const theme = localStorage.getItem("theme");
+    if (theme == "light") {
+        qs("body").replaceClass("dark", "light");
+        qs("#themeToggle").attr("checked", true);
+    }
+
     // Dark and light mode change
     qs("#themeToggle").onChange((e) => {
-        if(qs("#themeToggle").attr("checked") == true) qs("body").replaceClass("light", "dark");
-        else qs("body").replaceClass("dark", "light");
+        qs(":root").firstElement().style.setProperty("--transition-time", "0.2s");
+        if(qs("#themeToggle").attr("checked") == false){
+            qs("body").replaceClass("light", "dark");
+            localStorage.setItem("theme", "dark");
+        }
+        else {
+            qs("body").replaceClass("dark", "light");
+            localStorage.setItem("theme", "light");
+        }
+    })
+
+
+    // Sharp and rounded mode change
+    const border = localStorage.getItem("border");
+    if (border == "sharp") {
+        qs("body").replaceClass("border", "sharp");
+        qs("#borderRadiusToggle").attr("checked", true);
+    }
+
+    qs("#borderRadiusToggle").onChange((e) => {
+        qs(":root").firstElement().style.setProperty("--transition-time", "0.8s");
+        if(qs("#borderRadiusToggle").attr("checked") == true){
+            qs("body").replaceClass("round", "sharp");
+            localStorage.setItem("border", "sharp");
+        }
+        else {
+            qs("body").replaceClass("sharp", "round");
+            localStorage.setItem("border", "round");
+        }
     })
     
 
     
+
 })
